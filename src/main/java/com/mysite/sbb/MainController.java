14831d871f24a87a0710c88a61483d4c6a3c3174
윤희찬 <yhc920923@gmail.com>
2022-12-31 15:16:03 +0900

    service

--- src/main/java/com/mysite/sbb/MainController.java
+++ src/main/java/com/mysite/sbb/MainController.java
@@ -13,4 +13,9 @@ public class MainController {
         System.out.println("index");
         return "index";
     }
+
+    @GetMapping("/")
+    public String root() {
+        return "redirect:/question/list";
+    }
 }
