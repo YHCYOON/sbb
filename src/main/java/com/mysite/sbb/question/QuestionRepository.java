19997641c83434c2760c6105e5da280fc78f0bbc
윤희찬 <yhc920923@gmail.com>
2023-01-08 14:29:17 +0900

    paging

--- src/main/java/com/mysite/sbb/question/QuestionRepository.java
+++ src/main/java/com/mysite/sbb/question/QuestionRepository.java
@@ -1,9 +1,9 @@
 package com.mysite.sbb.question;
 
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.awt.print.Pageable;
 import java.util.List;
 
 public interface QuestionRepository extends JpaRepository<Question, Integer> {
